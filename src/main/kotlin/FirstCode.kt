99eccde6afb63d5ecd0cf36257eb0f5a261187a5
rmukha <mukhametdinov_1999@mail.ru>
2026-04-10 00:52:24 +0500

    10.04

---
+++ src/main/kotlin/FirstCode.kt
@@ -0,0 +1,14 @@
+fun main() {
+    /*print("Введите имя: " );
+    val name= readLine();
+    println("Ваше имя: $name")*/ //Вывод с помощью строки ввода
+
+    val d = (11>5) and (15>1); //оператор AND вывдит true или false
+    println(d);
+
+    val c = (4 < 5) or (9 > 10); //оператор OR вывдит true если одно = true
+    println(c);
+
+
+}
+
