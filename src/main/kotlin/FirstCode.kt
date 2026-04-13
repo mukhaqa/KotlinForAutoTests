bc2a41444c2d0b0619d0189547d3a307cbe0c8bb
rmukha <mukhametdinov_1999@mail.ru>
2026-04-13 22:48:07 +0500

    10.04

--- src/main/kotlin/FirstCode.kt
+++ src/main/kotlin/FirstCode.kt
@@ -1,7 +1,7 @@
 fun main() {
     /*print("Введите имя: " );
     val name= readLine();
-    println("Ваше имя: $name")*/ //Вывод с помощью строки ввода
+    println("Ваше имя: $name") //Вывод с помощью строки ввода
 
     val d = (11>5) and (15>1); //оператор AND вывдит true или false
     println(d);
@@ -9,6 +9,22 @@ fun main() {
     val c = (4 < 5) or (9 > 10); //оператор OR вывдит true если одно = true
     println(c);
 
+
+    val a = 10;
+    val b = 20;
+    println("Ответ: ${a+b}") */
+
+    val errorCode = 404
+
+    when (errorCode) {
+        200 -> println("✅ Успешно")
+        400 -> println("❌ Ошибка клиента")
+        401 -> println("❌ Не авторизован")
+        403 -> println("❌ Доступ запрещен")
+        404 -> println("❌ Не найдено")
+        500 -> println("❌ Ошибка сервера")
+        else -> println("⚠️ Неизвестная ошибка")
+    }
 
 }
 
